6ecd6cda132401e830c0a0d48f7d9d142cd5b28a
Timothy J Hickey <tjhickey@brandeis.edu>
2017-06-30 14:51:39 -0400

    added github links

---
+++ pages/gracehopper/client/gracehopper.js
@@ -0,0 +1,26 @@
+var students = [
+    {port:4200, name:'Alvarado, Joshua', github:'jAlva7'},
+    {port:4300, name:'Browning, Beckett', github:'beckettb'},
+    {port:4400, name:'Chen, Steven', github:'stchen'},
+    {port:4500, name:'Downs, Matthew', github:'Elysian1196'},
+    {port:4600, name:'Guan, Rong', github:'rguan971'},
+    {port:4700, name:'Guo, Yaxin', github:'yaxinguo'},
+    {port:4800, name:'He, Zhuoyuan', github:'hebuer'},
+    {port:4900, name:'Holderness, Eben', github:'eholderness95'},
+    {port:5100, name:'Ma, Cindy', github:'mcindy96'},
+    {port:5200, name:'Mandal, Nila', github:'nilamandal'},
+    {port:5300, name:'Marcus, Molly', github:'mmarcus11'},
+    {port:5400, name:'Matthews, R', github:'rrmatthews'},
+    {port:5500, name:'Rios, Jarrett', github:'hisnameisjack'},
+    {port:5600, name:'Sun, Claire', github:'csuncodes'},
+    {port:5700, name:'Wei, Weiwei', github:'ww199766'},
+    {port:5800, name:'Xu, Nina', github:'NinaXu317'},
+    {port:5900, name:'Zhang, Kaiyi', github:'kyzzzhang'},
+    {port:6100, name:'Zhou, April', github:'apzlx'},
+    {port:6200, name:'Zhu, Katherine', github:'katherinezyb'},
+];
+
+Template.gracehopper.helpers({
+	students(){return students},
+
+});
